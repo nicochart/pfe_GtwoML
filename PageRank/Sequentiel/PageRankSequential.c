58403b69f5e55c3536a888abb3e6bd32cb12b838
nicochart <76011128+nicochart@users.noreply.github.com>
2021-11-18 17:44:25 +0100

    MatricesCreuses & PageRank : Cas particuliers
    - Correction du passage d'un format COO à CSR : Cas où une ligne de la matrice est "vide" (remplie de 0)
    - PageRank : cas particuliers où on utilise beta = 1

--- PageRank/Sequentiel/PageRankSequential.c
+++ PageRank/Sequentiel/PageRankSequential.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 float random_between_0_and_1()
 {
@@ -75,13 +76,22 @@ void coo_to_csr_matrix(int *COO_Row, int *CSR_Row, long len_coo_row)
     COO_Column=CSR_Column, COO_Value=CSR_Value donc on le nes passe pas en paramètre.
     Le vecteur COO_Row est de longueur len_coo_row, et le vecteur CSR_Row est de longueur "nombre de ligne de la matrice + 1" (l+1).
     */
-    long i,current_indl = COO_Row[0];
-    printf("len_coo_row = %i\n",len_coo_row);
+    long i,current_indl = 0;
+    while(COO_Row[0] != current_indl) //cas particulier : première ligne de la matrice remplie de 0 (<=> indice de la première ligne, 0, différent du premier indice de ligne du vecteur Row)
+    {
+        *(CSR_Row + current_indl) = 0;
+        current_indl++;
+    }
     for (i=0;i<len_coo_row;i++)
     {
         if (COO_Row[i] != current_indl)
         {
             *(CSR_Row + current_indl + 1) = i;
+            while (COO_Row[i] != current_indl + 1) //cas particulier : ligne de la matrice vide (<=> indice de Row qui passe d'un nombre i à un nombre j supérieur à i+1)
+            {
+                current_indl++;
+                *(CSR_Row + current_indl + 1) = i;
+            }
             current_indl = COO_Row[i];
         }
     }
@@ -148,7 +158,7 @@ void fill_matrix_column_sum_vector(int *sum_vector, int *Column, int *Value, lon
     {
         *(sum_vector+i) = 0;
     }
-    
+
     for (i=0;i<len_values;i++) //on parcours le vecteur Column et Value, et on ajoute la valeur à la somme de la colonne correspondante
     {
         *(sum_vector + Column[i]) += Value[i];
@@ -185,6 +195,16 @@ void matrix_vector_product(double *y, double *A, double *x, int n)
     }
 }
 
+int one_in_vector(double *vect, int size)
+{
+    //retourne 1 s'il y a un "1" dans le vecteur (permet de tester un cas particulier du PageRank lorsque beta = 1)
+    for (int i=0;i<size;i++)
+    {
+        if (vect[i] == 1.0) {return 1;}
+    }
+    return 0;
+}
+
 double vector_norm(double *vect, int size)
 {
     double sum=0;
@@ -199,9 +219,12 @@ double abs_two_vector_error(double *vect1, double *vect2, int size)
 {
     /*Calcul l'erreur entre deux vecteurs de taille "size"*/
     double sum=0;
-    for (int i=0;i<size;i++) {sum += vect1[i] - vect2[i];}
-    if (sum < 0) {return - sum;}
-    else {return sum;}
+    for (int i=0;i<size;i++)
+    {
+        sum += fabs(vect1[i] - vect2[i]);
+        //printf("%f - %f ; sum = %f\n",vect1[i],vect2[i],sum);
+    }
+    return sum;
 }
 
 void copy_vector_value(double *vect1, double *vect2, int size)
@@ -230,18 +253,19 @@ void iterationMP(double *P, double *new_q, double *old_q, int n, double beta)
     for (i=0;i<n;i++) {new_q[i] *= 1/norme_new_q;}
 }
 
-int methodeDeLaPuissance(double *P, double *q_init, double *new_q, int n, double beta, double epsilon, int maxIter)
+int methodeDeLaPuissance(double *P, double *q_init, double *q_end, int n, double beta, double epsilon, int maxIter)
 {
     /*
     Applique la méthode de la puissance au vecteur initial q_init passé en paramètre, avec la matrice de passage P passée en paramètre
     */
     int i,cpt = 0;
+    double *new_q = (double *)malloc(n * sizeof(double));
     double *old_q = (double *)malloc(n * sizeof(double));
     double *tmp;
     copy_vector_value(q_init,old_q,n); //old_q = q_init
     copy_vector_value(q_init,new_q,n); //new_q = q_init
-    for (i=0;i<n;i++) {new_q[i] *= 1000;} //init pour avoir une différence
-    while (abs_two_vector_error(new_q,old_q,n) > epsilon && cpt<maxIter)
+    for (i=0;i<n;i++) {old_q[i] *= 1000;} //init pour avoir une différence
+    while (abs_two_vector_error(new_q,old_q,n) > epsilon && !one_in_vector(new_q,n) && cpt<maxIter)
     {
         /*old_q = new_q <=> copy_vector_value(new_q,old_q,n)*/
         tmp = new_q;
@@ -251,6 +275,8 @@ int methodeDeLaPuissance(double *P, double *q_init, double *new_q, int n, double
         iterationMP(P, new_q, old_q, n, beta);
         cpt++;
     }
+    copy_vector_value(new_q,q_end,n); //q_end = new_q
+    free(new_q);free(old_q);
     return cpt;
 }
 
@@ -275,7 +301,7 @@ int main(int argc, char **argv)
     long long size;
     int nb_zeros,nb_non_zeros;
     int *A;
-    
+
     //variables de la matrice creuse COO et CSR
     //COO et CSR
     int *Value,*Column;
@@ -283,29 +309,36 @@ int main(int argc, char **argv)
     int *COO_Row;
     //CSR
     int *Row;
-    
+
     //variables pour la normalisation de la matrice
     int *VectSum_Column;
     double *NormValue;
-    
+
     if (argc < 2)
     {
         printf("Veuillez entrer la taille de la matrice après le nom de l'executable : %s n\n", argv[0]);
         exit(1);
     }
-    
+
     n = atoll(argv[1]);
     size = n * n;
     A = (int *)malloc(size * sizeof(int));
-    
+
     for (i=0;i<n;i++)
     {
         init_row_dense_matrix(A, i, n, 75);
     }
-    
+    for (i=0;i<n;i++) //remplissage de la diagonale de 0
+    {
+        A[i*(n+1)] = 0;
+    }
+
+    printf("Matrice stockée \"dense\" :\n");
+    for (i=0;i<n;i++){for (j=0;j<n;j++){printf("%i ",A[i*n+j]);} printf("\n");}
+
     nb_zeros = cpt_nb_zeros_matrix(A, size);
     nb_non_zeros = size - nb_zeros;
-    
+
     COO_Row = (int *)malloc(nb_non_zeros * sizeof(int));
     Row = (int *)malloc((n+1) * sizeof(int));
     Column = (int *)malloc(nb_non_zeros * sizeof(int));
@@ -313,22 +346,22 @@ int main(int argc, char **argv)
 
     dense_to_coo_matrix(A, COO_Row, Column, Value, n, n);
     coo_to_csr_matrix(COO_Row, Row, nb_non_zeros);
-    
+
     VectSum_Column = (int *)malloc(n * sizeof(int));
     fill_matrix_column_sum_vector(VectSum_Column, Column, Value, nb_non_zeros, n);
-    
+
     NormValue = (double *)malloc(nb_non_zeros * sizeof(double));
     //copie du vecteur Value dans NormValue
     for(i=0;i<nb_non_zeros;i++) {NormValue[i] = (double) Value[i];}
-    
+
     //normalisation de la matrice (Row, Column, Value) dans (Row, Column, NormValue)
     normalize_matrix(VectSum_Column, Column, NormValue, nb_non_zeros, n);
-    
+
     if (debug)
     {
         printf("Nombre de zeros : %i\n",nb_zeros);
         printf("Nombre de valeurs non nulles : %i\n",nb_non_zeros);
-        
+
         printf("\nVecteur COO_Row :\n");
         for(i=0;i<nb_non_zeros;i++) {printf("%i ",COO_Row[i]);}
         printf("\nVecteur Row :\n");
@@ -339,28 +372,32 @@ int main(int argc, char **argv)
         for(i=0;i<nb_non_zeros;i++) {printf("%i ",Value[i]);} printf("\n");
         printf("\nMatrice creuse stockée en format CSR:\n");
         for (i=0;i<n;i++){for (j=0;j<n;j++){printf("%i ",get_csr_matrix_value_int(i, j, Row, Column, Value, n, n));} printf("\n");}
-        
-        printf("\nVecteur VectSum_Column (somme des éléments colonne par colonne:\n");
+
+        printf("\nVecteur VectSum_Column (somme des éléments colonne par colonne):\n");
         for(i=0;i<n;i++){printf("%i ",VectSum_Column[i]);}printf("\n");
-        
+
+        printf("\nVecteur NormValue:\n");
+        for (i=0;i<nb_non_zeros;i++){printf("%.2f ",NormValue[i]);} printf("\n");
+
         printf("\nMatrice normalisée sur les colonnes (stockée en format CSR):\n");
         for (i=0;i<n;i++){for (j=0;j<n;j++){printf("%.2f ",get_csr_matrix_value_double(i, j, Row, Column, NormValue, n, n));} printf("\n");}
         printf("\n");
     }
-    
+
     /*Page Rank*/
     double beta;
     double *q,*P;
-    int nb_iterations_faites;
-    
-    beta = 0.8;
+    int maxIter = 100000,nb_iterations_faites;
+    double epsilon = 0.00000000001;
+
+    beta = 1;
     q = (double *)malloc(n * sizeof(double));
     for (i=0;i<n;i++) {q[i] = (double) 1/n;}
-    
+
     P = (double *)malloc(size * sizeof(double));
     csr_to_dense_matrix(P, Row, Column, Value, n, n);
-    
-    nb_iterations_faites = methodeDeLaPuissance(P, q, q, n, beta, 0.00001, 1000);
+
+    nb_iterations_faites = methodeDeLaPuissance(P, q, q, n, beta, epsilon, maxIter);
     for(i=0;i<n;i++) {printf("%f ",q[i]);}
     printf("obtenu en %i itérations\n",nb_iterations_faites);
 }
